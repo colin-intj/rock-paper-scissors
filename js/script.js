0101d68de29414c26af20008a297ac595e130c90
Colin Fiedorowicz <70108727+colin-intj@users.noreply.github.com>
2022-06-15 13:29:40 -0500

    Add function to select computer's move

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+/**
+ * Randomly selects the computer's move for the game Rock Paper Scissors.
+ * @return {string} 'Rock', 'Paper', or 'Scissors'
+ */
+const computerPlay = () => ['Rock', 'Paper', 'Scissors'][~~(Math.random() * 2)];
