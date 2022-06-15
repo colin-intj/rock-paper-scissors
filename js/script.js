d84568715b3e7b7ae42539c77c1f9d9d8b07a1a3
Colin Fiedorowicz <70108727+colin-intj@users.noreply.github.com>
2022-06-15 14:27:17 -0500

    Add function that capitalizes first letter of word
    This commit adds a function that'll eventually modify user input so it
    uses the correct casing.

--- js/script.js
+++ js/script.js
@@ -6,5 +6,14 @@
  */
 const computerPlay = () => ['Rock', 'Paper', 'Scissors'][~~(Math.random() * 2)];
 
+/**
+ * This function capitalizes the first letter in a word and decapitalizes the
+ * rest.
+ * @param {string} word
+ * @return {string}
+ */
+const capitalizeFirstLetter =
+    (word) => word[0].toUpperCase() + word.slice(1).toLowerCase();
+
 const playRound = (playerSelection, computerSelection) => {
 };
