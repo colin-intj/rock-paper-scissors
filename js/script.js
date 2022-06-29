8a0e40d3a0f606d9f73ccdb322f8be3e9f81a910
Colin Fiedorowicz <70108727+colin-intj@users.noreply.github.com>
2022-06-29 15:30:24 -0500

    Add class to represent round results
    This commit adds a class used to generate results for each round.
    Storing the round's results in an object makes it easier to access
    individual properties such as who won and which move each player chose.

--- js/script.js
+++ js/script.js
@@ -1,41 +1,52 @@
 'use strict';
 
 /**
+ * Generates results for a round of Rock Paper Scissors.
+ */
+class RockPaperScissors {
+  /**
+   * @param {string} playerSelection
+   * @param {string} computerSelection
+   */
+  constructor(playerSelection, computerSelection) {
+    this.playerSelection = playerSelection;
+    this.computerSelection = computerSelection;
+
+    this.winner = {
+      /*
+       * Outer keys represent the player's move
+       * Inner keys represent the computer's move
+       */
+      rock: {rock: 'Tie', paper: 'Computer', scissors: 'Player'},
+      paper: {rock: 'Player', paper: 'Tie', scissors: 'Computer'},
+      scissors: {rock: 'Computer', paper: 'Player', scissors: 'Tie'},
+    }[playerSelection][computerSelection];
+
+    switch (this.winner) {
+      case 'Player':
+        this.winningMove = playerSelection;
+        this.losingMove = computerSelection;
+        break;
+      case 'Computer':
+        this.winningMove = computerSelection;
+        this.losingMove = playerSelection;
+        break;
+      default:
+        this.winningMove = 'N/A';
+        this.losingMove = 'N/A';
+    }
+
+    this.message =
+        this.winningMove === 'N/A' ?
+            'Tie' : `${this.winningMove} beats ${this.losingMove}`;
+  }
+}
+
+/**
  * Randomly selects the computer's move for the game Rock Paper Scissors.
  * @return {string} 'rock', 'paper', or 'scissors'
  */
 const computerPlay = () => ['rock', 'paper', 'scissors'][~~(Math.random() * 2)];
-
-/**
- * Determines whether the player won or lost and returns a message containing
- * the game results.
- * @param {string} playerSelection
- * @param {string} computerSelection
- * @return {string}
- */
-const getGameResults = (playerSelection, computerSelection) => {
-  return {
-    /*
-     * Outer keys represent the player's move
-     * Inner keys represent the computer's move
-     */
-    rock: {
-      rock: 'Tie',
-      paper: 'You lose: paper beats rock',
-      scissors: 'You win: rock beats scissors',
-    },
-    paper: {
-      rock: 'You win: paper beats rock',
-      paper: 'Tie',
-      scissors: 'You lose: scissors beats paper',
-    },
-    scissors: {
-      rock: 'You lose: rock beats scissors',
-      paper: 'You win: scissors beats paper',
-      scissors: 'Tie',
-    },
-  }[playerSelection][computerSelection];
-};
 
 /**
  * This function capitalizes the first letter in a word and decapitalizes the
@@ -58,18 +69,18 @@ document.querySelector(`[data-player='computer'] > span`).textContent =
 
 for (const button of buttons) {
   button.onclick = () => {
-    const playerSelection = button.dataset.selection;
-    const computerSelection = computerPlay();
+    const roundResults =
+        new RockPaperScissors(button.dataset.selection, computerPlay());
 
-    const winner = getGameResults(playerSelection, computerSelection);
+    alert(`${roundResults.winner} Wins\n\n` +
+        `Player chose: ${roundResults.playerSelection}\n` +
+        `Computer chose: ${roundResults.computerSelection}\n\n` +
+        `${capitalizeFirstLetter(roundResults.message)}`);
 
-    alert(`You chose: ${playerSelection}\nComputer chose: ` +
-        `${computerSelection}\n${winner}`);
-
-    if (winner.startsWith('You win')) {
+    if (roundResults.winner === 'Player') {
       document.querySelector(`[data-player='player'] > span`).textContent =
           ++playerScore;
-    } else if (winner.startsWith('You lose')) {
+    } else if (roundResults.winner === 'Computer') {
       document.querySelector(`[data-player='computer'] > span`).textContent =
           ++computerScore;
     }
